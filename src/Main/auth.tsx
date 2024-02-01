0a5a0738a7b0eb403d4889eebaa04afb331807dc
Arman <theaiemperor@gmail.com>
2024-02-01 16:27:22 +0530

    Sign In

--- src/Main/auth.tsx
+++ src/Main/auth.tsx
@@ -4,12 +4,18 @@ const API_KEY = "FIREBASE-API-KEY"
 
 
 async function createUser(email: string, password: string) {
-    const BASE_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=`
-    const URL = BASE_URL + API_KEY
+    const URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=` + API_KEY
+    await axios.post(URL, {
+        email, password, returnSecureToken: true
+    })
+}
+
+async function loginUser(email: string, password: string) {
+    const URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=` + API_KEY
     await axios.post(URL, {
         email, password, returnSecureToken: true
     })
 }
 
 
-export {createUser};
+export {createUser, loginUser};
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,25 @@
 import AuthContent from '..//components/Auth/AuthContent';
 import {ScrollView} from "react-native";
+import {useState} from "react";
+import {signInInterface} from "../constants/Types";
+import {loginUser} from "../Main/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function LoginScreen() {
+    const [authenticating, setAuthenticating] = useState(false)
+
+    const signIn = async ({email, password}: signInInterface) => {
+        setAuthenticating(true)
+        await loginUser(email, password)
+        setAuthenticating(false)
+    }
+
+
     return <ScrollView>
-        <AuthContent isLogin onAuthenticate={() => {
-        }}/>
+        {
+            authenticating ? <LoadingOverlay message="Signing in...."/> :
+                <AuthContent isLogin onAuthenticate={signIn}/>
+        }
     </ScrollView>;
 }
 
